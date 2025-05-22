60fe509c1926641f645a7f27b865d3f77c9d1248
asaamkwa <akaabadeklouis25@gmail.com>
2025-05-22 09:41:49 +0000

    first commit

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,113 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+            I am a Web Developer and Networking Engineer.  I ❤️ coding which help me to develop applications to solve real life problems.
+              <br />
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple"> Node js, JAVA, React, Express, CSS, HTML, Wordpress and Photoshop. </b>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are building new &nbsp;
+              <i>
+                <b className="purple">Web Technologies and Products </b> and
+                also in areas related to{" "}
+                <b className="purple">
+                  Research into new Technologies.
+                </b>
+              </i>
+              <br />
+              <br />
+              I started learning to code when I was 17 years old because <b className="purple">I wanted to make my own games,</b>
+              <i>
+                <b className="purple">
+                  {" "}
+                  how the Internet actually work. Over time,
+                </b>
+              </i>
+              &nbsp; 
+              <i>
+                <b className="purple"> I have gained a wealth of experience designing and developing mobile and web applications.</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/asaamkwa"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://github.com/asaamkwa"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://github.com/asaamkwa"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://github.com/asaamkwa"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import schoolmanagementsystemImage from "../../Assets/Projects/schoolmanagementsystem.png";
+import growafricaformeImage from "../../Assets/Projects/growafricaforme.png";
+import suicide from "../../Assets/Projects/suicide.png";
+//import bitsOfCode from "../../Assets/Projects/blog.png";
+import ecommerceImage from "../../Assets/Projects/ecommerce.jpg"
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ecommerceImage}
+              isBlog={false}
+              title="E-Commerce Website"
+              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/asaamkwa"
+              demoLink="https://github.com/asaamkwa"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={growafricaformeImage}
+              isBlog={false}
+              title="Consultant Website"
+              description="I developed this website for them: 
+              GrowAfrica For Me is an Enterprise Support Organization (ESO) and a Ghanaian Company Limited by Guarantee, dedicated to empowering Africa’s next generation of entrepreneurs. Initially established in 2022 as MakeAfrica Innovation DigiLab, we rebranded on January 22, 2025, to reflect our broader vision of inclusive and sustainable enterprise growth across the continent."
+              ghLink="https://github.com/asaamkwa"
+              demoLink="https://growafricaforme.org/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={schoolmanagementsystemImage}
+              isBlog={false}
+              title="School Management System"
+              description="A full-featured web application designed to streamline administrative and academic operations for schools. The system includes modules for student enrollment, class management, subject allocation, marks entry, result generation, and user authentication. Built using Node.js, Express, MongoDB, and EJS, it provides an intuitive interface for administrators, teachers, and students to interact efficiently. The system supports real-time data updates, role-based access control, and responsive design for all device types."
+              ghLink="https://github.com/asaamkwa"
+              demoLink="https://github.com/asaamkwa"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Plant AI"
+              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              ghLink="https://github.com/asaamkwa"
+              demoLink="https://github.com/asaamkwa"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="Ai For Social Good"
+              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              ghLink="https://github.com/asaamkwa"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="Face Recognition and Emotion Detection"
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              ghLink="https://github.com/asaamkwa"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
